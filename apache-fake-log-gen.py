d993109e8fa4f6ff972f0fd1b11420f8c5affce3
Vinay Kakade <vinaykakade@gmail.com>
2024-03-14 07:32:51 +0530

    Better perf

--- apache-fake-log-gen.py
+++ apache-fake-log-gen.py
@@ -18,57 +18,26 @@ local = get_localzone()
 # log rotation
 
 
-class switch(object):
-    def __init__(self, value):
-        self.value = value
-        self.fall = False
-
-    def __iter__(self):
-        """Return the match method once, then stop"""
-        yield self.match
-        raise StopIteration
-
-    def match(self, *args):
-        """Indicate whether or not to enter a case suite"""
-        if self.fall or not args:
-            return True
-        elif self.value in args: # changed for v1.5, see below
-            self.fall = True
-            return True
-        else:
-            return False
-
 parser = argparse.ArgumentParser(__file__, description="Fake Apache Log Generator")
-parser.add_argument("--output", "-o", dest='output_type', help="Write to a Log file, a gzip file or to STDOUT", choices=['LOG','GZ','CONSOLE'] )
 parser.add_argument("--log-format", "-l", dest='log_format', help="Log format, Common or Extended Log Format ", choices=['CLF','ELF'], default="ELF" )
 parser.add_argument("--num", "-n", dest='num_lines', help="Number of lines to generate (0 for infinite)", type=int, default=1)
 parser.add_argument("--prefix", "-p", dest='file_prefix', help="Prefix the output file name", type=str)
 parser.add_argument("--sleep", "-s", help="Sleep this long between lines (in seconds)", default=0.0, type=float)
+parser.add_argument("--month", "-m", help="Month to use", default=1, type=int)
 
 args = parser.parse_args()
 
 log_lines = args.num_lines
 file_prefix = args.file_prefix
-output_type = args.output_type
 log_format = args.log_format
+month = args.month
 
 faker = Faker()
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
-otime = datetime.datetime.now()
+otime = datetime.datetime(2023, args.month, 1)
 
 outFileName = 'access_log_'+timestr+'.log' if not file_prefix else file_prefix+'_access_log_'+timestr+'.log'
-
-for case in switch(output_type):
-    if case('LOG'):
-        f = open(outFileName,'w')
-        break
-    if case('GZ'):
-        f = gzip.open(outFileName+'.gz','w')
-        break
-    if case('CONSOLE'): pass
-    if case():
-        f = sys.stdout
 
 response=["200","404","500","301"]
 
@@ -80,10 +49,7 @@ ualist = [faker.firefox, faker.chrome, faker.safari, faker.internet_explorer, fa
 
 flag = True
 while (flag):
-    if args.sleep:
-        increment = datetime.timedelta(seconds=args.sleep)
-    else:
-        increment = datetime.timedelta(seconds=random.randint(30, 300))
+    increment = datetime.timedelta(seconds=0.01)
     otime += increment
 
     ip = faker.ipv4()
@@ -100,10 +66,10 @@ while (flag):
     referer = faker.uri()
     useragent = numpy.random.choice(ualist,p=[0.5,0.3,0.1,0.05,0.05] )()
     if log_format == "CLF":
-        f.write('%s - - [%s %s] "%s %s HTTP/1.0" %s %s\n' % (ip,dt,tz,vrb,uri,resp,byt))
+        print('%s - - [%s %s] "%s %s HTTP/1.0" %s %s' % (ip,dt,tz,vrb,uri,resp,byt))
     elif log_format == "ELF": 
-        f.write('%s - - [%s %s] "%s %s HTTP/1.0" %s %s "%s" "%s"\n' % (ip,dt,tz,vrb,uri,resp,byt,referer,useragent))
-    f.flush()
+        print('%s - - [%s %s] "%s %s HTTP/1.0" %s %s "%s" "%s"' % (ip,dt,tz,vrb,uri,resp,byt,referer,useragent))
+    
 
     log_lines = log_lines - 1
     flag = False if log_lines == 0 else True
